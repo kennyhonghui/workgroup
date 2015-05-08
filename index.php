72a3ce0197322ece8737711cf4e5189afa6d9d55
kennyhonghui <125828424@qq.com>
2015-05-08 10:56:07 +0800

    test complete

--- index.php
+++ index.php
@@ -19,5 +19,3 @@ $wgdb -> preare( 'insert into wg_test (test) values (?)', 2,3,4 );
 //$wgdb -> preare( 'insert into wg_test (test, test2) values (?,?)', [5,'a'],[6,'b'],[7,'c'] );
 $wgdb -> preare( 'insert into wg_test (test, test2) values (?,?)', array(5,'a'), array(6,'b'), array(7,'c') );
 //$wgdb -> preare( 'update wg_test set test2 = ? where test = ?', array(['d', 5]) );
-//$wgdb -> commit();
-echo 11;
